555a865780079af3700dff3d86076392167ede8a
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2021-01-12 17:51:46 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -67,20 +67,21 @@ var date = new Date();
 //Get Copyright
 document.getElementById("copy").innerHTML =  "Copyright © " + date.getFullYear() + " Pjotr Wisse";
 
-//this makes my birthday automatic
-// The date you were born
-let birthDate = new Date(2002, 5, 9, 0, 0, 0);
-// The current date
-let currentDate = date;
-// The age in years
-let age = currentDate.getFullYear() - birthDate.getFullYear();
-// Compare the months
-let month = currentDate.getMonth() - birthDate.getMonth();
-// Compare the days
-let day = currentDate.getDate() - birthDate.getDate();
-// if the date has already happend than change year
-if ( month < 0 || month === 0 && day < 0 )
-{
-	age--;
+function getAge(birth) {
+   var today = new Date();
+   var curr_date = today.getDate();
+   var curr_month = today.getMonth() + 1;
+   var curr_year = today.getFullYear();
+
+   var pieces = birth.split('/');
+   var birth_date = pieces[0];
+   var birth_month = pieces[1];
+   var birth_year = pieces[2];
+
+   if (curr_month == birth_month && curr_date >= birth_date) return parseInt(curr_year-birth_year);
+   if (curr_month == birth_month && curr_date < birth_date) return parseInt(curr_year-birth_year-1);
+   if (curr_month > birth_month) return parseInt(curr_year-birth_year);
+   if (curr_month < birth_month) return parseInt(curr_year-birth_year-1);
 }
+var age = getAge('05/09/2003');
 document.getElementById("age").innerHTML = age;
